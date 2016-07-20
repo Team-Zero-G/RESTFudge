951e543c1bd39ace8fb4e5a3e6d32fbefeba640a
KonstantinFarrell <konstantinfarrell@gmail.com>
2016-07-20 02:01:44 -0700

    Change to single file

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
-from flask import Flask
+import os
+from flask import Flask, request, redirect, url_for
 from flask_restful import Resource, Api
-from restfudge.main import RestFudge
 
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(RestFudge, '/')
+ALLOWED_EXTENSIONS = set(['jpg','png'])
+app.config['UPLOAD_FOLDER'] = 'data/'
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET','POST'])
+def index():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('index'))
+
+    template = open('index.html', 'r').read()
+    return str(template).join(os.listdir(app.config['UPLOAD_FOLDER']))
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
--- restfudge/main.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask_restful import Resource
-
-class RestFudge(Resource):
-    """ RESTful API for ImageFudge """
-
-    def post(self):
-        """ returns a fudged image as specified by request data """
-        pass
