c113cbb42524a58124d86ca8e64d48efbb9f0381
KonstantinFarrell <konstantinfarrell@gmail.com>
2016-07-20 12:06:28 -0700

    pep8 and documentation

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 import os
-from flask import Flask, request, redirect, url_for
-from flask_restful import Resource, Api
+from flask import Flask, request, redirect, url_for, render_template
+from flask_restful import Api
+from werkzeug import secure_filename
 
+# Initialize Flask
 app = Flask(__name__)
 api = Api(app)
 
-ALLOWED_EXTENSIONS = set(['jpg','png'])
+ALLOWED_EXTENSIONS = set(['jpg', 'png'])
 app.config['UPLOAD_FOLDER'] = 'data/'
 
 
 def allowed_file(filename):
+    ''' Returns true if a file is one of the allowed types '''
     return '.' in filename and \
             filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
-@app.route('/', methods=['GET','POST'])
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    # Handle file upload
     if request.method == 'POST':
         file = request.files['file']
         if file and allowed_file(file.filename):
@@ -22,8 +27,11 @@ def index():
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             return redirect(url_for('index'))
 
-    template = open('index.html', 'r').read()
-    return str(template).join(os.listdir(app.config['UPLOAD_FOLDER']))
+    # No file. Render index template.
+    return render_template(
+        'index.html',
+        data=os.listdir(app.config['UPLOAD_FOLDER'])
+    )
 
 
 if __name__ == '__main__':
