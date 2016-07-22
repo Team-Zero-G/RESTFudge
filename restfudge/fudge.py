bb8f9d08ede249b9308603ea8dca56570fe04683
KonstantinFarrell <konstantinfarrell@gmail.com>
2016-07-21 23:58:15 -0700

    pep8

--- restfudge/fudge.py
+++ restfudge/fudge.py
@@ -37,6 +37,31 @@ class FudgeAPIMeta(Resource):
                 ), 200, headers)
         return redirect(url_for('index'))
 
+    def post(self, guid, effect):
+        ''' Applies an effect on an image '''
+        if is_valid(guid) and effect is not None:
+            filename = get_file_from_guid(guid)
+            ext = filename.split('.')[1]
+            new_filename = '{guid}_{effect}.{extension}'
+            new_filename = new_filename.format(filename=guid,
+                                               effect=effect,
+                                               extension=ext)
+            fudged = self._fudge(filename, effect)
+            fudged.save('data/{}'.format(new_filename))
+            filename = new_filename
+        headers = {'Content-Type': 'text/html'}
+        return make_response(render_template(
+            'image.html',
+            filepath='data/{}'.format(new_filename),
+            filename=filename
+        ), 200, headers)
+
+    def _fudge(self, filename, effect, kwargs):
+        if is_supported(effect):
+            f = Fudged(filename)
+            # Is this terrible practice?
+            f.__dict__[effect](kwargs)
+            return f
 
 
 def is_supported(effect):
