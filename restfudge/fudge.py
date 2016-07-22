103c3a020b51f463b6d94d14ed4662062fdc3254
KonstantinFarrell <konstantinfarrell@gmail.com>
2016-07-21 23:51:48 -0700

    Add GET for API

--- restfudge/fudge.py
+++ restfudge/fudge.py
@@ -3,13 +3,14 @@ from flask import redirect, url_for, render_template, make_response
 from flask_restful import Resource
 from restfudge.settings import app
 
+from imagefudge.image_fudge import Fudged
+
 
 class FudgeMeta(Resource):
     """ Handles original images """
     def get(self, guid):
         if is_valid(guid):
-            files = os.listdir(app.config['UPLOAD_FOLDER'])
-            filename = [filename for filename in files if guid in filename][0]
+            filename = get_file_from_guid(guid)
             headers = {'Content-Type': 'text/html'}
             return make_response(render_template(
                 'image.html',
@@ -22,12 +23,36 @@ class FudgeMeta(Resource):
 
 class FudgeAPIMeta(Resource):
     def get(self, guid, effect):
-        ''' Returns an image if the particular effect has been applied '''
-        pass
+        ''' Returns an image if the particular effect has been applied.
+        Otherwise redirects to the index page.
+        '''
+        if is_valid(guid) and effect is not None:
+            filename = get_file_from_guid(guid, effect)
+            if filename is not None:
+                headers = {'Content-Type': 'text/html'}
+                return make_response(render_template(
+                    'image.html',
+                    filepath='data/{}'.format(filename),
+                    filename=filename
+                ), 200, headers)
+        return redirect(url_for('index'))
 
-    def post(self, guid, effect):
-        ''' Applies an effect on an image '''
-        pass
+
+
+def is_supported(effect):
+    return effect in [
+        "draw_relative_arcs",
+    ]
+
+
+def get_file_from_guid(guid, effect=None):
+    ''' Returns a file based on its guid '''
+    files = os.listdir(app.config['UPLOAD_FOLDER'])
+    if effect is None:
+        search = guid
+    else:
+        search = "{}_{}".format(guid, effect)
+    return next(filter(lambda x: search in x, files)) or None
 
 
 def is_valid(guid):
