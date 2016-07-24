267363aa2c7e41a1fe822d701e8b34d1f1bf2a66
KonstantinFarrell <konstantinfarrell@gmail.com>
2016-07-24 15:01:25 -0700

    Update tests

--- tests/tests.py
+++ tests/tests.py
@@ -1,6 +1,26 @@
+import os
+import signal
+import subprocess
 from unittest import TestCase
+from flask import request, redirect, url_for, render_template, make_response
+from restfudge.utils import allowed_file, slug
+from restfudge.fudge import FudgeMeta, FudgeAPIMeta
+from restfudge.settings import app
+from main import run, api
 
 
-class TestRESTFudgeAPI(TestCase):
-    def test_foo(self):
-        self.assertTrue(True)
+TEST_IMAGE = 'restfudge/static/data/15DE87EECEF79345239D20D4B0FB5D6C.png'
+
+
+class TestFudgeMeta(TestCase):
+    def setUp(self):
+        self.app = app
+        self.image = TEST_IMAGE
+        self.slug = self.image.replace('/','.').split('.')[-2]
+
+    def test_slug(self):
+        self.assertEqual(len(self.slug), 32)
+
+if __name__ == '__main__':
+    run(debug=True)
+    unittest.main()
